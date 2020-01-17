2dd6b40f25a9ee31d196de00d233a34c2a816b11
lvjx <lvjx@ushareit.com>
2020-01-17 22:35:15 +0800

    堆排序增加for循环方式

--- 堆排序/HeapSort.java
+++ 堆排序/HeapSort.java
@@ -36,6 +36,24 @@ class HeapSort {
 		arr[k] = guard;
 	}
 
+	//使用for循环的方式进行堆构造
+	public static void heapForBuilder(int[] arr, int s, int len){
+		int tmp = arr[s];
+		//j=2*s+1 是因为完全二叉树当前i节点的左孩子的序号一定是2*i，右孩子一定是2*i+1
+		//j*=2+1 它们的孩子当然也就是以2的位数序号增加，因此j变量才这样循环
+		for(int j=2*s+1;j<len;j*=2+1){//沿关键字较大的孩子节点向下筛选
+			if(j+1<len && arr[j]<arr[j+1]){
+				j++; //j为较大记录的下标
+			}
+			if(tmp>=arr[j])
+				break;
+
+			arr[s]=arr[j];//将较大的值赋值给s
+			s = j;	
+		}
+		arr[s] = tmp;//插入原始值
+	}
+
 	public static void heapSort(int[] arr){
 		//堆构造，从第一个非子叶节点开始(arr.length/2-1)
 		for(int i=arr.length/2-1;i>=0;i--){
